45c601cc8705324c638d1559f8b06ec01b9aea30
xanderkiev2022 <301019a@gmail.com>
2022-12-23 09:12:45 +0200

    more changes

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const BASE_URL = 'https://newsapi.org/v2';
+const options = {
+  headers: {
+    Authorization: API_KEY,
+  },
+};
+
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchArticles() {
+    const url = `${BASE_URL}/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
+
+    return fetch(url, options)
+      .then(response => response.json())
+      .then(({ articles }) => {
+        this.incrementPage();
+        return articles;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
