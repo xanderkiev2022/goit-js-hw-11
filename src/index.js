aefdb1814847a3ac33970b0125c117d92d59d1ee
Alex Koval <301019a@gmail.com>
2022-12-25 08:59:10 +0200

    more changess

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
 import PicApiService from './js/PicApiService';
 import './css/common.css';
 
 // import LoadMoreBtn from './js/components/load-more-btn';
 
-  const searchForm = document.querySelector('.search-form');
-  const gallery = document.querySelector('.gallery');
+const searchForm = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
 
-  searchForm.addEventListener('submit', onSearch);
-  window.addEventListener('scroll', infinitiScroll);
+searchForm.addEventListener('submit', onSearch);
+window.addEventListener('scroll', infinitiScroll);
 
-  const picApiService = new PicApiService();
-
-// const loadMoreBtn = new LoadMoreBtn({
-//   selector: '[data-action="load-more"]',
-//   hidden: true,
-// });
-
-// loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+const picApiService = new PicApiService();
 
 function onSearch(e) {
   e.preventDefault();
@@ -25,35 +18,35 @@ function onSearch(e) {
   picApiService.query = e.target.elements.searchQuery.value;
 
   if (picApiService.query === '') {
-    return alert('Введи что-то нормальное');
+    Notiflix.Notify.failure('Please, enter something to start searching');
   }
 
-  // loadMoreBtn.show();
   picApiService.resetPage();
   clearPicsContainer();
   fetchPics();
 }
 
 function fetchPics() {
-  // loadMoreBtn.disable();
   picApiService.fetchPictures().then(hits => {
+    // if (hits === 0) {
+    //   Notiflix.Notify.failure('agwgtwtaPlease, enter something to start searching');
+    // }
     appendPicsMarkup(hits);
-    // loadMoreBtn.enable();
   });
 }
 
 function appendPicsMarkup(hits) {
-  gallery.insertAdjacentHTML('afterbegin', picTpl(hits));
+  gallery.insertAdjacentHTML('beforeend', picTpl(hits));
 }
 
 function picTpl(hits) {
   return hits.reduce(
-    (acc, {webformatURL, likes, views, comments, downloads}) =>
+    (acc, { webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
       acc +
       `
       <div class="photo-card">
       <div class="wrapper">
-  <img src="${webformatURL}" alt="" loading="lazy" /></div>
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" /></div>
   <div class="info">
     <p class="info-item">
       <b>Likes: </b> ${likes}
@@ -77,10 +70,10 @@ function clearPicsContainer() {
   gallery.innerHTML = '';
 }
 
-function infinitiScroll (){
+function infinitiScroll() {
   const documentRect = document.documentElement.getBoundingClientRect();
-  if (documentRect.bottom < document.documentElement.clientHeight +150) {
-    picApiService.incrementPage();
+  if (documentRect.bottom < document.documentElement.clientHeight + 150) {
+    // picApiService.incrementPage();
     fetchPics();
   }
 }
--- src/js/PicApiService.js
+++ src/js/PicApiService.js
@@ -10,14 +10,27 @@ export default class PicApiService {
   }
 
   async fetchPictures() {
-    const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&page=${this.page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`
-      try {
-        const {data : {hits}} = await axios.get(url);
-        this.incrementPage();
-        return hits;
-      }
-      catch {(error => console.error(error))};
+    const searchParams = new URLSearchParams({
+      key: API_KEY,
+      q: this.searchQuery,
+      page: this.page,
+      per_page: 40,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    });
+    const url = `${BASE_URL}/?${searchParams}`;
+    // const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&page=${this.page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`;
+    try {
+      const {
+        data: { hits },
+      } = await axios.get(url);
+      this.incrementPage();
+      return hits;
+    } catch {
+      error => console.error(error);
     }
+  }
 
   incrementPage() {
     this.page += 1;
