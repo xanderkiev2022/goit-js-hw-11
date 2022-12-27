4c8452b1b4c9c3b12f9e13a19257a13ead52ac55
Alex Koval <301019a@gmail.com>
2022-12-27 12:55:46 +0200

    fin + extracts

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import PicApiServiceClass from './js/PicApiService';
 import createPicsMarkup from './js/PicTemplates';
 import './css/common.css';
 
 const searchForm = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
-const picApiService = new PicApiServiceClass('', 1, 40);
 let loading = false;
+
+const picApiService = new PicApiServiceClass('', 1, 40);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  animationSpeed: 250,
+  overlay: true,
+  close: true,
+  showCounter: true,
+  fadeSpeed: 250,
+  loop: false,
+});
 
 searchForm.addEventListener('submit', onSearch);
 window.addEventListener('scroll', infinitiScroll);
 
 function onSearch(e) {
   e.preventDefault();
+
   picApiService.query = e.target.elements.searchQuery.value;
 
-  if (picApiService.query === '') {
-    Notiflix.Notify.failure('Please, enter something to start searching');
-  }
+  if (picApiService.query === '') {Notiflix.Notify.failure('Please, enter something to start searching');}
 
-  // picApiService.resetPage();
+  picApiService.resetPage();
   clearPicsContainer();
   fetchPics();
 }
@@ -28,29 +40,19 @@ async function fetchPics() {
   loading = true;
   try {
     const pictures = await picApiService.fetchPictures();
-    const {
-      data: { hits, totalHits },
-    } = pictures;
+    const {data: { hits, totalHits },} = pictures;
 
-    appendPicsMarkup(hits);
-    if (!totalHits) {
-      // if (totalHits === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again'
-      );
-    }
-    // else if (numberOfPics >= totalHits && totalHits != 0) {
-    // else if (error.response.status === 400) {
-    // else if (!totalHits && !document.documentElement.getBoundingClientRect().bottom <= document.documentElement.clientHeight) {
-    // else if (document.documentElement.scrollHeight === window.pageYOffset + window.innerHeight) {
-    // else if (picApiService.page >= Math.ceil(totalHits / picApiService.per_page) {
-    else if (hits.length < picApiService.per_page) {
+    appendPicsMarkup(hits, totalHits);
+    lightbox.refresh();
+    smoothScroll();
+
+    if (hits.length < picApiService.per_page) {
       window.removeEventListener('scroll', infinitiScroll);
       window.addEventListener('scroll', infinitiScrollEnd);
     }
-  } catch (error) {
-    console.log(error.message);
-  } finally {
+  } 
+  catch (error) {console.log(error.message);} 
+  finally {
     searchForm.reset();
     loading = false;
   }
@@ -58,24 +60,21 @@ async function fetchPics() {
 
 function infinitiScrollEnd(e) {
   const { scrollTop, clientHeight } = document.documentElement;
-  if (
-    scrollTop + clientHeight + 100 >=
-    document.documentElement.getBoundingClientRect().height
-  ) {
-    Notiflix.Notify.failure(
-      "We're sorry, but you've reached the end of search results"
-    );
-    window.removeEventListener('scroll', infinitiScrollEnd);
-  }
+  if (scrollTop + clientHeight + 100 >=document.documentElement.getBoundingClientRect().height) {
+    Notiflix.Notify.failure("We're sorry, but you've reached the end of search results");
+    window.removeEventListener('scroll', infinitiScrollEnd);}
 }
 
-function appendPicsMarkup(hits) {
-  gallery.insertAdjacentHTML('beforeend', createPicsMarkup(hits));
-}
+function appendPicsMarkup(hits, totalHits) {
+  if (!totalHits) {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again');}
+  if (picApiService.page===1 && totalHits>=1){
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);}
+
+  gallery.insertAdjacentHTML('beforeend', createPicsMarkup(hits));}
 
 function clearPicsContainer() {
-  gallery.innerHTML = '';
-}
+  gallery.innerHTML = '';}
 
 function infinitiScroll() {
   if (!loading) {
@@ -87,7 +86,10 @@ function infinitiScroll() {
   }
 }
 
-// _________________
-// else {
-//   Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-// }
+function smoothScroll (){
+  const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 3,
+    behavior: "smooth",
+  });
+}
--- src/js/PicTemplates.js
+++ src/js/PicTemplates.js
@@ -4,23 +4,26 @@ export default function createPicsMarkup(hits) {
         acc +
         `
         <div class="photo-card">
-        <div class="wrapper">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" /></div>
-    <div class="info">
-      <p class="info-item">
-        <b>Likes: </b> ${likes}
-      </p>
-      <p class="info-item">
-        <b>Views: </b> ${views}
-      </p>
-      <p class="info-item">
-        <b>Comments: </b> ${comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads: </b> ${downloads}
-      </p>
-    </div>
-  </div>`,
+          <div class="wrapper">
+            <a href="${largeImageURL}">
+              <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            </a>
+          </div>
+          <div class="info">
+            <p class="info-item">
+              <b>Likes: </b> ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views: </b> ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments: </b> ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads: </b> ${downloads}
+            </p>
+          </div>
+        </div>`,
       ''
     );
   }
